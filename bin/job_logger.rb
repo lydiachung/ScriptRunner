112b9dd764a9fcc22623760beff7e6004518cc17
Lydia Chung <lchung@areteinc.com>
2013-11-07 17:13:21 -0500

    init commit.

---
+++ bin/job_logger.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+module JobLogger
+  
+  attr_accessor :s_log_file
+
+  def write_log(s_msg_type, s_message)
+    
+    # creates a directory and all its parent directories
+    s_dir_name = File.dirname(s_log_file)
+    FileUtils.mkdir_p(s_dir_name) if File.directory? s_dir_name
+    
+    unless s_message.empty?
+      File.open(@s_log_file, "a") do |o_file| 
+        o_file.write("[#{s_msg_type}] #{get_current_datetime_str()} #{s_message} \n") 
+      end
+    end
+  end
+  
+  def get_current_datetime_str()
+    return DateTime.now.strftime("%m/%d/%Y %H:%M:%S")
+  end
+  
+end
